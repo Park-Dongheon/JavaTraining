22bf4fd58f682b0cc36c08a632f9c445cfef694d
Park-Dongheon <cdsded@naver.com>
2024-04-29 02:30:21 +0900

    java_인터페이스 과제 수정

--- java_works/edu/src/com/ruby/java/ch07/abstraction/제출과제_7장_인터페이스.java
+++ java_works/edu/src/com/ruby/java/ch07/abstraction/제출과제_7장_인터페이스.java
@@ -3,57 +3,53 @@ package com.ruby.java.ch07.abstraction;
  * 7장 실습 코드로 구현한다 
  */
 
-interface Shape {									// Shape 인터페이스 구현
-	public double perimeter();						// 인터페이스 메소드, 컴파일 시 public abstract 제어자가 자동 추가, perimeter(): 둘레, area(): 면적 
-	public float area();
+interface Shape {									// Shape 인터페이스 정의, 도형의 둘레·면적 계산 메서드를 포함
+	public double perimeter();						// Shape 인터페이스에 선언된 메소드, 도형의 '둘레'를 계산하는 추상메서드, 컴파일 시 public abstract 제어자가 자동 추가
+	public float area();							// Shape 인터페이스에 선언된 메소드, 도형의 '면적'를 계산하는 추상메서드, 컴파일 시 public abstract 제어자가 자동 추가
 }
 
-class Triangle implements Shape {											// 삼각형 Triangle 클래스, Shape 인터페이스를 상속 받음
-	private int x1,y1,x2,y2,x3,y3;
+class Triangle implements Shape {											// 삼각형 Triangle 클래스 정의, Shape 인터페이스를 구현 => Shape 인터페이스의 모든 메서드 구현
+	private int x1,y1,x2,y2,x3,y3;											// Triangle 클래스 멤버 변수, 삼각형의 세 점의 좌표
 	
-	public Triangle(int x1, int y1, int x2, int y2, int x3, int y3) {		// 삼각형 Triangle 클래스의 생성자 메소드
+	public Triangle(int x1, int y1, int x2, int y2, int x3, int y3) {		// 삼각형 Triangle 클래스의 생성자 메서드, 세 점의 좌표를 입력받아 초기화
 		// TODO Auto-generated constructor stub
 		this.x1 = x1;
 		this.y1 = y1;
 		this.x2 = x2;
 		this.y2 = y2;
 		this.x3 = x3;
-		this.y2 = y3;
+		this.y3 = y3;
 	}
 	
 	@Override
-	public double perimeter() {												// 둘레값 계산, 세 점의 좌표를 받아 피타고라스 원리로 세 변의 길이를 구함, 계산된 값을 반환함
-		double sum = 0;
-	
+	public double perimeter() {												// 삼각형의 둘레 계산, '피타고라스 원리'로 세 변의 길이를 구함, 계산된 값을 반환함
+																			// 한 변의 길이 계산, 제곱함수(Math.pow) · 제곱근함수(Math.sqrt) 이용
 		double side1 = Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
 		double side2 = Math.sqrt(Math.pow(x2 - x3, 2) + Math.pow(y2 - y3, 2));
 		double side3 = Math.sqrt(Math.pow(x3 - x1, 2) + Math.pow(y3 - y1, 2));
-		return sum = side1 + side2 + side3;
+		return side1 + side2 + side3;
 	
 	}
 	
 	@Override
-	public float area() {													// 넓아값 계산, 삼각형의 세 번의 길이를 이용해 삼각형의 넓이를 구하는 헤론의 공식 사용,
-																			// 계산된 값을 반환함
-		float sum = 0;
+	public float area() {													// 삼각형의 면적 계산, 삼각형의 세 번의 길이를 이용해 삼각형의 넓이를 구하는 '헤론의 공식' 사용, 계산된 값을 반환함 
 		
 		float side1 = (float) Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
 		float side2 = (float) Math.sqrt(Math.pow(x2 - x3, 2) + Math.pow(y2 - y3, 2));
 		float side3 = (float) Math.sqrt(Math.pow(x3 - x1, 2) + Math.pow(y3 - y1, 2));
-		float sideHalf = (side1 + side2 + side3)/2;
+		float sideHalf = (side1 + side2 + side3) / 2;						// 헤론의 공식에 사용되는 둘레 절반값
 		
-		return sum = (float) Math.sqrt(sideHalf * (sideHalf - side1) * (sideHalf - side2) * (sideHalf - side3));
+		return (float) Math.sqrt(sideHalf * (sideHalf - side1) * (sideHalf - side2) * (sideHalf - side3));
 	}
 	
 }
 
-class Rectangle implements Shape {											// 직사각형 Rectangle 클래스, Shape 인터페이스를 상속 받음
+class Rectangle implements Shape {											// 직사각형 Rectangle 클래스 정의, Shape 인터페이스를 구현
 	
 	int x1,y1,x2,y2;
 	
-	public Rectangle(int x1, int y1, int x2, int y2) {						// 직대각선의 두 점의 좌표(대각 좌표)
+	public Rectangle(int x1, int y1, int x2, int y2) {						// Rectangle 클래스 생성자 메서드, 직사각형의 대각선 두 점의 좌표를 입력받아 초기화 
 		// TODO Auto-generated constructor stub
-//		super(x1,y1,x2,y2,x3,y3);
 		this.x1 = x1;
 		this.y1 = y1;
 		this.x2 = x2;
@@ -61,10 +57,8 @@ class Rectangle implements Shape {											// 직사각형 Rectangle 클래스
 	}
 
 	@Override
-	public double perimeter() {
+	public double perimeter() {												// 직사각형의 둘레 계산, 절댓값 함수(Math.abs)이용							
 		// TODO Auto-generated method stub
-		
-		double diagonal = 0;												// 직사각형의 대각선의 길이
 		double side1 = Math.abs(x1 - x2);
 		double side2 = Math.abs(y1 - y2);
 		
@@ -72,7 +66,7 @@ class Rectangle implements Shape {											// 직사각형 Rectangle 클래스
 	}
 
 	@Override
-	public float area() {
+	public float area() {													// 직사각형의 면적 계산
 		// TODO Auto-generated method stub
 		
 		float side1 = Math.abs(x1 - x2);
@@ -82,45 +76,35 @@ class Rectangle implements Shape {											// 직사각형 Rectangle 클래스
 	
 }
 
-class Square extends Rectangle {
-
-		int x1, y1;
+class Square extends Rectangle {											// 정사각형 Square 클래스 정의, Rectangle 클래스를 상속(Rectangle 클래스의 모든 멤버를 상속 받음)
 		int side;
 		
-		public Square(int x1, int y1, int x2) {
-			super(x1, y1, x2, x2);
-			this.x1 = x1;
-			this.y1 = y1;
-			this.side = Math.abs(x1 - x2);
+		public Square(int x1, int y1, int side) {							// Square 클래스 생성자 메서드, 사각형의 한 꼭지점의 좌표와 변의 길이를 입력받아 초기화 ex) (x1, y1) = (1, 2) / (x1 + side, y1 + side) = (4, 5)
+			super(x1, y1, x1 + side, y1 + side);
+			this.side = side;
 		}
 		
 		@Override
-		public double perimeter() {
+		public double perimeter() {											// 정사각형의 둘레 계산												
 			// TODO Auto-generated method stub
-			
-			double side1 = Math.abs(x1 - x2);
-			double side2 = Math.abs(y1 - y2);
-			
-			return 2 * (side1 + side2);
+			return 4 * side;
 		}
 
 		@Override
-		public float area() {
+		public float area() {												// 정사각형의 면적 계산
 			// TODO Auto-generated method stub
 			
-			float side1 = Math.abs(x1 - x2);
-			float side2 = Math.abs(y1 - y2);
-			return side1 * side2;
+			return side * side;
 		}
 		
 }
 
 public class 제출과제_7장_인터페이스 {
 	public static void main(String[] args) {
-		Shape t = new Triangle(1,2,3,4,5,6);
-		Shape r = new Rectangle(1,2,3,4);
-		Shape s = new Square(1,2,3);
-		System.out.println("삼각형 둘레 길이 = " + t.perimeter());
+		Shape t = new Triangle(1,2,3,4,5,6);								// Shape 인터페이스를 구현한 Triangle 클래스의 객체를 생성
+		Shape r = new Rectangle(1,2,3,4);									// Shape 인터페이스를 구현한 Rectangle 클래스의 객체를 생성
+		Shape s = new Square(1,2,3);										// Shape 인터페이스를 구현한 Square 클래스의 객체를 생성
+		System.out.println("삼각형 둘레 길이 = " + t.perimeter());				// 참조변수를 통한 메서드 호쿨, 반환값을 출력
 		System.out.println("삼각형 면적 = " + t.area());
 		System.out.println("직사각형 둘레 길이 = " + r.perimeter());
 		System.out.println("직사각형 면적 = " + r.area());
