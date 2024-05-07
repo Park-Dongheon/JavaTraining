a9294bdbb9a3e95d3152392baa5e89ec0dfd0932
Park-Dongheon <cdsded@naver.com>
2024-05-08 00:00:55 +0900

    Lotto_mod

---
+++ java_works/edu/src/chapter14/Test01.java
@@ -0,0 +1,24 @@
+package chapter14;
+
+interface MyInterface {
+	
+	public void print();
+	
+}
+
+class MyClass1 implements MyInterface {
+
+	@Override
+	public void print() {
+		System.out.println();
+	}
+	
+}
+
+public class Test01 {
+
+	public static void main(String[] args) {
+
+	}
+
+}
--- java_works/edu/src/com/ruby/java/ch10/collection/제출과제_Test_ch10_lotto당첨처리_수정.java
+++ java_works/edu/src/com/ruby/java/ch10/collection/제출과제_Test_ch10_lotto당첨처리_수정.java
@@ -13,19 +13,21 @@ package com.ruby.java.ch10.collection;
 */
 //hashset을 arraylist로 변환하여 정렬하기 구현
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Random;
 
-class ListComparator2 implements Comparator<List<Integer>>{					// 비교, Comparator 인터페이스 클래스 사용
+class ListComparator implements Comparator<List<Integer>>{					// 비교, Comparator 인터페이스 사용
 	@Override
 	public int compare(List<Integer> l1, List<Integer> l2) {
 		Iterator<Integer> ait = l1.iterator();
 		Iterator<Integer> bit = l2.iterator();
 		while (ait.hasNext()) {
-			int anum = ait.next();int bnum = bit.next();
+			int anum = ait.next();
+			int bnum = bit.next();
 			if ( anum > bnum) return 1;
 			else if (anum < bnum) return -1;
 			
@@ -34,7 +36,7 @@ class ListComparator2 implements Comparator<List<Integer>>{					// 비교, Compa
 	}
 }
 
-public class 제출과제_Test_ch10_lotto당첨처리 {
+public class 제출과제_Test_ch10_lotto당첨처리_수정 {
 
 	public static void main(String[] args) {
 
@@ -60,17 +62,26 @@ public class 제출과제_Test_ch10_lotto당첨처리 {
 			//구현할 부분
 			/*
 			   새로운 HashSet<Integer> 객체 생성, lotto 변수가 참조, HashSet에 사이즈가 0~5 총 6개의 Random한 요소값을 저장,
-			   HashSet<HashSet<Integer>> 타입의 lot에 HashSet<Integer> 타입인 lotto를 추가한다.
+			   HashSet<HashSet<Integer>> 타입의 lot HashSet객체에 HashSet<Integer> 타입인 lotto HashSet객체를 추가한다.
 			*/
 			lotto = new HashSet<Integer>();					// 복권 1장
-			for (int j = 0; lotto.size() < 6; j++) {
+			for (int j = 0; lotto.size() < 7; j++) {
 				lotto.add(number.nextInt(45) + 1);
 			}
-			System.out.println(lotto);
 			lot.add(lotto);
 		}
 		
+		/*
+		   HashSet<HashSet> 컬렉션 객체인 lot를 iterator() 메서드를 이용해 Iterator 객체로 변환하여 반환함, 
+		   Iterator의 제네릭 타입을 HashSet<Iteger> 형태로 선언하고 lotList 참조변수에 lot객체를 저장 
+		   iter.hasNext()는 현재 커서 다음에 데이터가 존재하는지 판단 후 iter.next() 메서드로 커서 다음의 데이터를 반환하고 커서를 다음 데이터로 이동(HashSet 컬렉션의 요소인 lotto를 순차적으로 출력) 
+		*/
+		Iterator<HashSet<Integer>> lotList = lot.iterator();
+		while(lotList.hasNext()) {
+			System.out.println(lotList.next());
+		}
 		System.out.println("\nlot hashset을 출력\n");
+		
 		for (HashSet<Integer> eachLotto : lot) {
 			/*
 			 * 33  1 17 22  6  8  + 보너스번호: 12
@@ -78,43 +89,38 @@ public class 제출과제_Test_ch10_lotto당첨처리 {
 			 */
 			//구현할 부분 - ArrayList 사용
 			/*
-			   HashSet 컬렉션 객체인 eachLotto를 iterator() 메서드를 이용해 Iterator 객체로 변환하여 반환함, 
-			   iterator() 메서드는 Collection 인터페이스에서 선언한 메서드이므로 List, Set 계열의 모든 컬렉션에서 사용 가능
-			   iter.hasNext()는 현재 커서 다음에 데이터가 존재하는지 판단 후 iter.next() 메서드로 커서 다음의 데이터를 반환하고 커서를 다음 데이터로 이동
-			   eachLotto 컬렉션의 데이터를 순차적으로 접근하여 출력
+			   ArrayList<Integer> 타입의 객체 list를 생성, list에 HashSet<Integer> 타입의 eachLotto를 저장,
+			   list 객체는 로또 번호를 요소로 가짐, list를 순회하면서 번호 6개를 출력하고 마지막 요소를 보너스 번호로 출력 
 			*/
-			Iterator<Integer> iter = eachLotto.iterator();
-			while(iter.hasNext()) {
-				System.out.print(iter.next() + " ");
+			ArrayList<Integer> list = new ArrayList<Integer>();
+			list.addAll(eachLotto);
+			for(int i = 0; i < list.size()-1; i++) {
+				System.out.print(list.get(i) + " ");
 			}
-			// 보너스 번호를 중복되지 않게 추가
-			int bonus = 0;
-			for(int k = 0; k < 1; k++) {					// 보너스 번호 1나만 추가
-				bonus = number.nextInt(45) + 1;				
-				if(!eachLotto.contains(bonus)) {			// eachLotto의 요소 중 bonus 번호를 포함하지 않으면 반복
-					eachLotto.add(bonus);
-				}
-			}
-			System.out.println("+ 보너스번호 [" + bonus + "]");
+			System.out.println("+ 보너스번호 [" + list.get(6) + "]");
 			/*
 			   al은 List<List<Integer>> 타입의 리스트, al에는 List<Integer> 타입의 요소만 추가, 
 			   HashSet<Integer> eachLotto 변수는 List<Integer> 타입이 아니므로 직접 추가x -> (eachLotto) HashSet 컬렉션을 받아 ArrayList 객체로 새로 생성, 
 			   List<Iteger> 타입의 eachLottoList를 변수에 객체를 저장하여, List<List<Integer>> 타입 al에 추가
 			*/
-			List<Integer> eachLottoList = new ArrayList<>(eachLotto);
+			List<Integer> eachLottoList = new ArrayList<Integer>(eachLotto);
 			al.add(eachLottoList);
 		}
 		
 		System.out.println("복권 정렬전::lot = " + al);
 		
-		al.sort(new ListComparator2());
-		System.out.println("복권 정렬후::lot = " + al);
 		//hashset의 리스트를 정렬하는 알고리즘 개발
-		//hashset를 arrayList로 변환
+		//hashset를 arrayList로 변환		->		al 객체의 요소 List를 각각 뽑아 정렬
+		for(int i = 0; i < al.size(); i++) {
+			Collections.sort(al.get(i));
+		}
+		al.sort(new ListComparator());
+		System.out.println("복권 정렬후::lot = " + al);
+
 		//당첨번호 추첨
 		HashSet<Integer> win = new HashSet<>();
-		for (int j = 0; win.size() < 7; j++) {//6개 번호 set에 넣음 중복체크, 보너스 번호 별도
-			win.add(number.nextInt(46));
+		for (int m = 0; win.size() < 7; m++) {//6개 번호 set에 넣음 중복체크, 보너스 번호 별도
+			win.add(number.nextInt(45) + 1);
 		}
 		System.out.print("당첨번호: " + win);//6개의 당첨번호와 보너스번호
 		// 6개를 맞힌 복권을 찾는다 
